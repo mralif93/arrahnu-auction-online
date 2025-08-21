eef075ec0adc662b840f706aa05247e0d1e1aca8
Muhammad Alif Bin Tajudin <mr.alif.93@gmail.com>
2025-08-21 16:02:01 +0800

    Update database driver in configuration from PostgreSQL to MySQL for Google Cloud SQL, aligning with project requirements.

--- config/database.php
+++ config/database.php
@@ -95,7 +95,7 @@ return [
         ],
 
         'google_cloud_sql' => [
-            'driver' => 'pgsql',
+            'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'arrahnu_sit_db'),
